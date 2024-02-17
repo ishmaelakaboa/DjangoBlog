92f11b0e789ca7d413118fbad6869794eced94ce
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2024-02-17 04:56:31 +0000

    set up django broswer reloads

--- myblog/models.py
+++ myblog/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     slug = models.SlugField(max_length=250)
+    publish = models.DateTimeField(default=timezone.now)
+    craeyed = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        odering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
     
     def __str__(self):
         return self.title
