8495c6c274259dc023adb71ae14197db1b0dce07
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2024-02-19 16:29:17 +0000

    add posts list and details views

--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.http import Http404
 
-def post_list()
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'myblog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found")
+    return render(request, 'myblog/post/detail.html', {'posts': posts})
+    
